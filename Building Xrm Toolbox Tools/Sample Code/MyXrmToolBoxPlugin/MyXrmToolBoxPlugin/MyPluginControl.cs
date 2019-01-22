7ed4530f3d794cfec724b39a78faef62f704f952
James Novak <james.novak@gmail.com>
2019-01-21 21:41:24 -0500

    initial check in
    sample project and slides for presentation

---
+++ Building Xrm Toolbox Tools/Sample Code/MyXrmToolBoxPlugin/MyXrmToolBoxPlugin/MyPluginControl.cs
@@ -0,0 +1,229 @@
+﻿using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+
+namespace MyXrmToolBoxPlugin
+{
+    public partial class MyPluginControl : PluginControlBase
+    {
+        private Settings mySettings;
+
+        public MyPluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void tsbSample_Click(object sender, EventArgs e)
+        {
+            // The ExecuteMethod method handles connecting to an
+            // organization if XrmToolBox is not yet connected
+            ExecuteMethod(GetRoles);
+        }
+
+        private void GetRoles()
+        {
+            WorkAsync(new WorkAsyncInfo()
+            {
+                Message = "Retrieving Roles",
+                AsyncArgument = null,
+                Work = (worker, args) =>
+                {
+                    var QErole = new QueryExpression("role");
+
+                    QErole.ColumnSet.AddColumns("name", "businessunitid", "roleid");
+
+                    args.Result = Service.RetrieveMultiple(QErole);
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+
+                    listBoxSecurityRoles.Items.Clear();
+
+                    if (result != null)
+                    {
+                        var items = new List<ListBoxDisplayItem>();
+                        foreach (var ent in result.Entities)
+                        {
+                            items.Add(new ListBoxDisplayItem((string)ent["name"], ent.Id.ToString()));
+                        }
+                        listBoxSecurityRoles.DataSource = items.OrderBy(e => e.Name).ToList();
+                        listBoxSecurityRoles.DisplayMember = "Name";
+                        listBoxSecurityRoles.ValueMember = "Value";
+                    }
+                }
+            });
+        }
+
+        private class ListBoxDisplayItem
+        {
+            internal ListBoxDisplayItem(string name, string value)
+            {
+                _name = name;
+                _value = value;
+            }
+
+            private string _name;
+            private string _value;
+
+            public string Name { get => _name; set => _name = value; }
+            public string Value { get => _value; set => _value = value; }
+        }
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        private void listBoxSecurityRoles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ExecuteMethod(LoadUsers);
+        }
+
+        private void LoadUsers()
+        {
+            var role = listBoxSecurityRoles.SelectedItem as ListBoxDisplayItem;
+
+            if (role == null)
+                return;
+
+            var id = role.Value;
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Getting System Users",
+                AsyncArgument = id,
+                Work = (worker, args) =>
+                {
+                    var roleId = (string)args.Argument;
+
+            // Instantiate QueryExpression QEsystemuser
+            var QEsystemuser = new QueryExpression("systemuser");
+
+            // Add columns to QEsystemuser.ColumnSet
+            QEsystemuser.ColumnSet.AddColumns("internalemailaddress", "lastname", "firstname", "domainname");
+
+            // Add link-entity QEsystemuser_systemuserroles
+            var QEsystemuser_systemuserroles = QEsystemuser.AddLink("systemuserroles", "systemuserid", "systemuserid");
+                    QEsystemuser_systemuserroles.EntityAlias = "sur";
+
+            // Add link-entity QEsystemuser_systemuserroles_role
+            var QEsystemuser_systemuserroles_role = QEsystemuser_systemuserroles.AddLink("role", "roleid", "roleid");
+                    QEsystemuser_systemuserroles_role.EntityAlias = "role";
+
+            // Add columns to QEsystemuser_systemuserroles_role.Columns
+            QEsystemuser_systemuserroles_role.Columns.AddColumns("name");
+
+            // Define filter QEsystemuser_systemuserroles_role.LinkCriteria
+            QEsystemuser_systemuserroles_role.LinkCriteria.AddCondition("roleid", ConditionOperator.Equal, roleId);
+
+
+                    args.Result = Service.RetrieveMultiple(QEsystemuser);
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.Message, "Oh crap", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (args.Result is EntityCollection views)
+                    {
+                        crmGridView.DataSource = views;
+                    }
+                }
+            });
+        }
+
+        private void crmGridView_RecordClick(object sender, Cinteros.Xrm.CRMWinForm.CRMRecordEventArgs e)
+        {
+            if (e.Value is EntityReference entref)
+            {
+                OpenEntityReference(entref);
+            }
+        }
+
+        private void crmGridView_RecordDoubleClick(object sender, Cinteros.Xrm.CRMWinForm.CRMRecordEventArgs e)
+        {
+            OpenEntityReference(e.Entity.ToEntityReference());
+        }
+
+        private void OpenEntityReference(EntityReference entref)
+        {
+            if (!string.IsNullOrEmpty(entref.LogicalName) && !entref.Id.Equals(Guid.Empty))
+            {
+                var url = ConnectionDetail.WebApplicationUrl;
+                if (string.IsNullOrEmpty(url))
+                {
+                    url = string.Concat(ConnectionDetail.ServerName, "/", ConnectionDetail.Organization);
+                    if (!url.ToLower().StartsWith("http"))
+                    {
+                        url = string.Concat("http://", url);
+                    }
+                }
+                url = string.Concat(url,
+                    url.EndsWith("/") ? "" : "/",
+                    "main.aspx?etn=",
+                    entref.LogicalName,
+                    "&pagetype=entityrecord&id=",
+                    entref.Id.ToString());
+                if (!string.IsNullOrEmpty(url))
+                {
+                    Process.Start(url);
+                }
+            }
+        }
+    }
+}
---
+++ Building Xrm Toolbox Tools/Sample Code/MyXrmToolBoxPlugin/MyXrmToolBoxPlugin/MyPluginControl.designer.cs
@@ -0,0 +1,160 @@
+﻿namespace MyXrmToolBoxPlugin
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbSample = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBoxSecurityRoles = new System.Windows.Forms.ListBox();
+            this.crmGridView = new Cinteros.Xrm.CRMWinForm.CRMGridView();
+            this.toolStripMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.crmGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1,
+            this.tsbSample});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStripMenu.Size = new System.Drawing.Size(1445, 39);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(171, 36);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 39);
+            // 
+            // tsbSample
+            // 
+            this.tsbSample.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSample.Name = "tsbSample";
+            this.tsbSample.Size = new System.Drawing.Size(91, 36);
+            this.tsbSample.Text = "Try me";
+            this.tsbSample.Click += new System.EventHandler(this.tsbSample_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 39);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBoxSecurityRoles);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.crmGridView);
+            this.splitContainer1.Size = new System.Drawing.Size(1445, 998);
+            this.splitContainer1.SplitterDistance = 480;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // listBoxSecurityRoles
+            // 
+            this.listBoxSecurityRoles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxSecurityRoles.FormattingEnabled = true;
+            this.listBoxSecurityRoles.ItemHeight = 25;
+            this.listBoxSecurityRoles.Location = new System.Drawing.Point(0, 0);
+            this.listBoxSecurityRoles.Name = "listBoxSecurityRoles";
+            this.listBoxSecurityRoles.Size = new System.Drawing.Size(480, 998);
+            this.listBoxSecurityRoles.TabIndex = 6;
+            this.listBoxSecurityRoles.SelectedIndexChanged += new System.EventHandler(this.listBoxSecurityRoles_SelectedIndexChanged);
+            // 
+            // crmGridView
+            // 
+            this.crmGridView.AllowUserToAddRows = false;
+            this.crmGridView.AllowUserToDeleteRows = false;
+            this.crmGridView.AllowUserToOrderColumns = true;
+            this.crmGridView.AllowUserToResizeRows = false;
+            this.crmGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.crmGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crmGridView.EntityReferenceClickable = true;
+            this.crmGridView.FilterColumns = "";
+            this.crmGridView.Location = new System.Drawing.Point(0, 0);
+            this.crmGridView.Name = "crmGridView";
+            this.crmGridView.ReadOnly = true;
+            this.crmGridView.RowTemplate.Height = 31;
+            this.crmGridView.ShowIdColumn = false;
+            this.crmGridView.Size = new System.Drawing.Size(961, 998);
+            this.crmGridView.TabIndex = 0;
+            this.crmGridView.RecordClick += new Cinteros.Xrm.CRMWinForm.CRMRecordEventHandler(this.crmGridView_RecordClick);
+            this.crmGridView.RecordDoubleClick += new Cinteros.Xrm.CRMWinForm.CRMRecordEventHandler(this.crmGridView_RecordDoubleClick);
+            // 
+            // MyPluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStripMenu);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(1445, 1037);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.crmGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBoxSecurityRoles;
+        private Cinteros.Xrm.CRMWinForm.CRMGridView crmGridView;
+    }
+}
